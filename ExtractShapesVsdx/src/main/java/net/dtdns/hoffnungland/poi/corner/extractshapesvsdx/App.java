71e1ab2eb59683a5f069d80dbecd06c47a6eebed
manuelmsperanza <manuel.m.speranza@gmail.com>
2018-04-24 22:12:24 +0200

    hoffnungland

---
+++ ExtractShapesVsdx/src/main/java/net/dtdns/hoffnungland/poi/corner/extractshapesvsdx/App.java
@@ -0,0 +1,77 @@
+package net.dtdns.hoffnungland.poi.corner.extractshapesvsdx;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.poi.xdgf.usermodel.XDGFPage;
+import org.apache.poi.xdgf.usermodel.XDGFShape;
+import org.apache.poi.xdgf.usermodel.XmlVisioDocument;
+
+/**
+ * Print out all the shape text split by sheet
+ *
+ */
+public class App 
+{
+	private static Logger logger = LogManager.getLogger(App.class);
+
+	public static void main( String[] args )
+	{
+
+
+		try {
+			logger.info("Initialize the list of visio");
+			FileFilter visioFilter = new FileFilter(){
+
+				@Override
+				public boolean accept(File pathname) {
+					if(pathname.isFile()){
+						if(pathname.getName().endsWith(".vsdx")){
+							return true;
+						}
+					}
+					return false;
+				}
+
+			};
+
+			File visioDir = new File(".");
+			for (File curFile : visioDir.listFiles(visioFilter)){
+				logger.debug("Loading " + curFile.getName());
+				//File visioFile = new File("");
+				FileInputStream visioFileIn = new FileInputStream(curFile);
+				XmlVisioDocument visioDoc = new XmlVisioDocument(visioFileIn);
+
+				for (XDGFPage curPage : visioDoc.getPages()){
+					
+					String pageName = curPage.getName().trim().replaceAll("^\\[(\\w+)\\]:?\\s", "$1\t");
+					
+					for(XDGFShape curShape : curPage.getContent().getShapes()){
+						if(curShape.getText() != null){
+							String shapeText = curShape.getText().getTextContent().trim().replace('‘', '\'')
+									.replace('’', '\'')
+									.replace('“', '"')
+									.replace('”', '"')
+									.replaceAll("\\s+", " ")
+									.replaceAll("\n", "\t")
+									.replaceAll("^(\\w+\\d+):?\\s", "$1\t");
+							
+							logger.info(pageName + "\t" + shapeText);
+						}
+					}
+				}
+
+				visioDoc.close();
+				visioFileIn.close();
+
+			}
+
+		} catch (IOException e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+}
--- OrcXlsReport/README.md
+++ OrcXlsReport/README.md
@@ -1,4 +1,4 @@
 #Create a new project
-mvn archetype:generate -DarchetypeCatalog=http://repo.maven.apache.org/maven2/archetype-catalog.xml -Dfilter=maven-archetype-quickstart -DgroupId=net.dtdns.hoffunungland -DartifactId=OrcXlsReport -Dpackage=net.dtdns.hoffunungland.poi.corner.orcxlsreport -Dversion=0.0.1-SNAPSHOT
+mvn archetype:generate -DarchetypeCatalog=http://repo.maven.apache.org/maven2/archetype-catalog.xml -Dfilter=maven-archetype-quickstart -DgroupId=net.dtdns.hoffnungland -DartifactId=OrcXlsReport -Dpackage=net.dtdns.hoffnungland.poi.corner.orcxlsreport -Dversion=0.0.1-SNAPSHOT
 #Build settings
 ##Remove junit:junit:3.8.1
