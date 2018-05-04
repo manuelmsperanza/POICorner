a33987a71a6972ee14ff19247c673496f92985ae
manuelmsperanza <manuel.m.speranza@gmail.com>
2018-05-04 11:52:01 +0200

    Fixed query: remove LISTAGG

--- OrcXlsReport/OracleDBA92/queries/FRAMEWORK INDEXES.sql
+++ OrcXlsReport/OracleDBA92/queries/FRAMEWORK INDEXES.sql
@@ -1,7 +1,6 @@
-select ui.table_name, ui.index_name, list_cols.column_list
-from user_indexes ui, (select TABLE_NAME, INDEX_NAME,  LISTAGG(column_name||' '||DESCEND,', ') WITHIN GROUP (ORDER BY COLUMN_POSITION) column_list
-from user_ind_columns
-group by table_name, index_name) list_cols
+select ui.table_name, ui.index_name, uic.column_name, uic.DESCEND, uic.COLUMN_POSITION
+from user_indexes ui, user_ind_columns uic 
 where ui.index_type <> 'LOB'
-and ui.index_name = list_cols.INDEX_NAME (+)
+and ui.index_name = uic.INDEX_NAME (+)
 and not exists (select 1 from user_constraints uc where uc.index_name = ui.index_name)
+order by table_name, index_name, COLUMN_POSITION
