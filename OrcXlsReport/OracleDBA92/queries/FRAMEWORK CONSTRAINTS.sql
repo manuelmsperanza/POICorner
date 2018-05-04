ac2e3baebcaa135f3778b1eeb36dc2dd856a90c4
manuelmsperanza <manuel.m.speranza@gmail.com>
2018-05-04 11:52:01 +0200

    Fixed query: remove LISTAGG

--- OrcXlsReport/OracleDBA92/queries/FRAMEWORK CONSTRAINTS.sql
+++ OrcXlsReport/OracleDBA92/queries/FRAMEWORK CONSTRAINTS.sql
@@ -1,24 +1,17 @@
-select list_cols.table_name, DECODE(uc.constraint_type, 'P', 'PK', 'U', 'UNIQUE', '?')  CONSTRAINT_TYPE, list_cols.constraint_name, list_cols.column_list, uc.R_CONSTRAINT_NAME column_list_ref, uc.SEARCH_CONDITION, uc.DEFERRABLE, uc.DEFERRED, uc.VALIDATED
-from user_constraints uc, (select ucc.table_name, ucc.constraint_name, LISTAGG(ucc.column_name,', ') WITHIN GROUP (ORDER BY ucc.position) column_list
-from user_cons_columns ucc
-group by ucc.table_name, ucc.constraint_name) list_cols
+select ucc.table_name, DECODE(uc.constraint_type, 'P', 'PK', 'U', 'UNIQUE', '?')  CONSTRAINT_TYPE, ucc.constraint_name, ucc.column_name, uc.R_CONSTRAINT_NAME column_list_ref, uc.SEARCH_CONDITION, uc.DEFERRABLE, uc.DEFERRED, uc.VALIDATED
+from user_constraints uc, user_cons_columns ucc
 where uc.constraint_type in('P', 'U')
-and uc.constraint_name = list_cols.constraint_name
+and uc.constraint_name = ucc.constraint_name
 UNION ALL
-select list_cols.table_name, 'CHECK' CONSTRAINT_TYPE, list_cols.constraint_name, list_cols.column_list, uc.R_CONSTRAINT_NAME column_list_ref, uc.SEARCH_CONDITION, uc.DEFERRABLE, uc.DEFERRED, uc.VALIDATED
-from user_constraints uc, (select ucc.table_name, ucc.constraint_name, LISTAGG(ucc.column_name,', ') WITHIN GROUP (ORDER BY ucc.position) column_list
-from user_cons_columns ucc
-group by ucc.table_name, ucc.constraint_name) list_cols
+select ucc.table_name, 'CHECK' CONSTRAINT_TYPE, ucc.constraint_name, ucc.column_name, uc.R_CONSTRAINT_NAME column_list_ref, uc.SEARCH_CONDITION, uc.DEFERRABLE, uc.DEFERRED, uc.VALIDATED
+from user_constraints uc, user_cons_columns ucc
 where uc.constraint_type = 'C'
-and uc.constraint_name = list_cols.constraint_name
+and uc.constraint_name = ucc.constraint_name
 UNION ALL
-select a.table_name, 'FK' CONSTRAINT_TYPE, a.constraint_name, a.column_list, b.table_name||' ('||b.column_list||')' column_list_ref, uc.SEARCH_CONDITION, uc.DEFERRABLE, uc.DEFERRED, uc.VALIDATED
-from user_constraints uc, (select ucc.table_name, ucc.constraint_name, LISTAGG(ucc.column_name,', ') WITHIN GROUP (ORDER BY ucc.position) column_list
-from user_cons_columns ucc
-group by ucc.table_name, ucc.constraint_name) a, (select ucc.table_name, ucc.constraint_name, LISTAGG(ucc.column_name,', ') WITHIN GROUP (ORDER BY ucc.position) column_list
-from user_cons_columns ucc
-group by ucc.table_name, ucc.constraint_name) b
+select ucca.table_name, 'FK' CONSTRAINT_TYPE, ucca.constraint_name, ucca.column_name, uccb.table_name||' ('||uccb.column_name||')' column_list_ref, uc.SEARCH_CONDITION, uc.DEFERRABLE, uc.DEFERRED, uc.VALIDATED
+from user_constraints uc, user_cons_columns ucca, user_cons_columns uccb 
 where uc.constraint_type = 'R'
-and uc.constraint_name = a.constraint_name
-and uc.R_CONSTRAINT_NAME = b.constraint_name
+and uc.constraint_name = ucca.constraint_name
+and uc.R_CONSTRAINT_NAME = uccb.constraint_name
+and ucca.position = uccb.position
 order by table_name, constraint_name
