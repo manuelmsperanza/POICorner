395a93a88fe62e9d505023c34a91239ad31282a9
manuel.m.speranza <manuel.m.speranza@accenture.com>
2019-05-07 16:40:20 +0200

    first version PDFCreator

---
+++ PDFCreator/src/main/java/me/hoffnungland/poi/corner/pdfcreator/App.java
@@ -0,0 +1,35 @@
+package me.hoffnungland.poi.corner.pdfcreator;
+
+import java.io.FileNotFoundException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfVersion;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.WriterProperties;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+public class App 
+{
+	private static final Logger logger = LogManager.getLogger(App.class);
+	
+    public static void main( String[] args )
+    {
+    	logger.traceEntry();
+		try {
+			PdfWriter writer = new PdfWriter("Test.pdf", new WriterProperties().setPdfVersion(PdfVersion.PDF_2_0));
+			PdfDocument pdfDocument = new PdfDocument(writer);
+	    	pdfDocument.setTagged();
+	    	Document document = new Document(pdfDocument);
+	    	document.add(new Paragraph("Hello world!"));
+	    	document.close();
+		} catch (FileNotFoundException e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.traceExit();
+    	
+    }
+}
---
+++ PDFCreator/src/test/java/me/hoffnungland/poi/corner/pdfcreator/AppTest.java
@@ -0,0 +1,20 @@
+package me.hoffnungland.poi.corner.pdfcreator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
