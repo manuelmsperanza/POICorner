cebc3f9e28a4a9ec71c1891dcd1206a2eed5e5de
manuelmsperanza <manu.speranz@gmail.com>
2022-03-11 19:17:42 +0100

    tabular json into excel

--- JSONXlsLoader/src/main/java/com/hoffnungland/poi/corner/jsonxlsloader/App.java
+++ JSONXlsLoader/src/main/java/com/hoffnungland/poi/corner/jsonxlsloader/App.java
@@ -17,6 +17,9 @@ import org.json.JSONArray;
 import org.json.JSONML;
 import org.json.JSONObject;
 
+import com.hoffnungland.poi.corner.dbxlsreport.ExcelManager;
+import com.hoffnungland.poi.corner.dbxlsreport.XlsWrkSheetException;
+
 import java.awt.Component;
 import javax.swing.Box;
 import java.awt.Dimension;
@@ -34,6 +37,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JFormattedTextField;
 import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
@@ -45,17 +49,11 @@ public class App implements ActionListener{
 	private JTextField jsonTextField;
 	private JTextField targetDirTextField;
 	private JTextField xlsxNameTextField;
-
-	private JRadioButton flatRadioButton;
-
-	private JRadioButton differentSheetsRadioButton;
-
-	private JCheckBox skipArrayCheckBox;
 	
 	private static final String LOAD_JSON_ACTION = "Load JSON Action";
 	private static final String TARGET_DIR_ACTION = "Targer DIR Action";
 	private static final String CONVERT_ACTION = "Convert Action";
-
+	private File[] selectedJsonFiles;
 	/**
 	 * Launch the application.
 	 */
@@ -116,6 +114,7 @@ public class App implements ActionListener{
 		frame.getContentPane().add(loadJsonButton);
 		
 		jsonTextField = new JTextField();
+		jsonTextField.setEditable(false);
 		springLayout.putConstraint(SpringLayout.NORTH, jsonTextField, 1, SpringLayout.NORTH, loadJsonButton);
 		springLayout.putConstraint(SpringLayout.WEST, jsonTextField, 10, SpringLayout.EAST, loadJsonButton);
 		springLayout.putConstraint(SpringLayout.EAST, jsonTextField, -10, SpringLayout.EAST, frame.getContentPane());
@@ -148,35 +147,13 @@ public class App implements ActionListener{
 		springLayout.putConstraint(SpringLayout.EAST, xlsxNameTextField, -10, SpringLayout.EAST, frame.getContentPane());
 		frame.getContentPane().add(xlsxNameTextField);
 		xlsxNameTextField.setColumns(10);
-		
-		flatRadioButton = new JRadioButton("One Sheet Flat");
-		flatRadioButton.setSelected(true);
-		springLayout.putConstraint(SpringLayout.NORTH, flatRadioButton, 15, SpringLayout.SOUTH, xlsxNameLabel);
-		springLayout.putConstraint(SpringLayout.WEST, flatRadioButton, 10, SpringLayout.WEST, frame.getContentPane());
-		frame.getContentPane().add(flatRadioButton);
-		
-		differentSheetsRadioButton = new JRadioButton("Separate sheets");
-		springLayout.putConstraint(SpringLayout.NORTH, differentSheetsRadioButton, 0, SpringLayout.NORTH, flatRadioButton);
-		springLayout.putConstraint(SpringLayout.WEST, differentSheetsRadioButton, 10, SpringLayout.EAST, flatRadioButton);
-		frame.getContentPane().add(differentSheetsRadioButton);
-		
-		ButtonGroup radioBtnGroup = new ButtonGroup();
-        radioBtnGroup.add(flatRadioButton);
-        radioBtnGroup.add(differentSheetsRadioButton);
-		
-        skipArrayCheckBox = new JCheckBox("Skip non-object array");
-        springLayout.putConstraint(SpringLayout.NORTH, skipArrayCheckBox, 0, SpringLayout.NORTH, differentSheetsRadioButton);
-        springLayout.putConstraint(SpringLayout.WEST, skipArrayCheckBox, 10, SpringLayout.EAST, differentSheetsRadioButton);
-		frame.getContentPane().add(skipArrayCheckBox);
         
 		JButton convertButton = new JButton("Convert");
 		springLayout.putConstraint(SpringLayout.NORTH, convertButton, 12, SpringLayout.SOUTH, xlsxNameTextField);
-		springLayout.putConstraint(SpringLayout.WEST, convertButton, 10, SpringLayout.EAST, skipArrayCheckBox);
+		springLayout.putConstraint(SpringLayout.WEST, convertButton, 371, SpringLayout.WEST, frame.getContentPane());
 		convertButton.setActionCommand(CONVERT_ACTION);
 		convertButton.addActionListener(this);
 		frame.getContentPane().add(convertButton);
-		
-		
 		
 		logger.traceExit();
 	}
@@ -207,7 +184,7 @@ public class App implements ActionListener{
 		
 		JFileChooser fc = new JFileChooser();
 		JsonFilter fcJsonFiler = new JsonFilter();
-		fc.setMultiSelectionEnabled(false);
+		fc.setMultiSelectionEnabled(true);
 		fc.setFileFilter(fcJsonFiler);
 		fc.addChoosableFileFilter(fcJsonFiler);
 		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -215,12 +192,30 @@ public class App implements ActionListener{
 		int returnVal = fc.showOpenDialog(this.frame);
 		
 		if(returnVal == JFileChooser.APPROVE_OPTION) {
-			String jsonFilePath = fc.getSelectedFile().getPath();
-			this.jsonTextField.setText(fc.getSelectedFile().getPath());
-			String jsonFolderPath = jsonFilePath.substring(0, jsonFilePath.lastIndexOf(File.separator));
+			this.selectedJsonFiles = fc.getSelectedFiles();
+				
+			File selectedFile = this.selectedJsonFiles[0];
+			String jsonFilePath = selectedFile.getPath();
+			String jsonFolderPath = jsonFilePath.substring(0, jsonFilePath.lastIndexOf(File.separator)+1);
 			this.targetDirTextField.setText(jsonFolderPath);
-			String jsonFileName = fc.getSelectedFile().getName();
-			this.xlsxNameTextField.setText(jsonFileName.substring(0, jsonFileName.lastIndexOf(".")) + ".xlsx");
+			
+			if(this.selectedJsonFiles.length == 1) {
+				String jsonFileName = selectedFile.getName();
+				this.xlsxNameTextField.setText(jsonFileName.substring(0, jsonFileName.lastIndexOf(".")) + ".xlsx");				
+			} else {
+				this.xlsxNameTextField.setText(Path.of(jsonFolderPath).getFileName() + ".xlsx");
+			}
+			
+			String jsonTextFieldStr = null;
+			for(File curJsonFile :  this.selectedJsonFiles) {
+				if(jsonTextFieldStr == null) {
+					jsonTextFieldStr = curJsonFile.getPath();
+				} else {
+					jsonTextFieldStr += ";" + curJsonFile.getName();
+				}
+			}
+			this.jsonTextField.setText(jsonTextFieldStr);
+			
 		}
 		
 		logger.traceExit();
@@ -236,7 +231,11 @@ public class App implements ActionListener{
 		int returnVal = fc.showOpenDialog(this.frame);
 		
 		if(returnVal == JFileChooser.APPROVE_OPTION) {
-			this.targetDirTextField.setText(fc.getSelectedFile().getPath());
+			String targetPath = fc.getSelectedFile().getPath();
+			if(!targetPath.endsWith(File.separator)) {
+				targetPath += File.separator;
+			}
+			this.targetDirTextField.setText(targetPath);
 		}
 		
 		logger.traceExit();
@@ -246,127 +245,34 @@ public class App implements ActionListener{
 	private void convertJsonToExcel() {
 		
 		logger.traceEntry();
+		ExcelManager xlsMng = null;
 		try {
-			String jsonStr = Files.readString(Path.of(this.jsonTextField.getText()));
-			
-			Map<String, ColumnHeader> mapColumnHeader = new HashMap<String, ColumnHeader>();
-			List<CellHierarchy> listCells = new ArrayList<CellHierarchy>();
-
-			if(jsonStr.startsWith("{")) {
-			
-				JSONObject jsonObj = new JSONObject(jsonStr);
-
-				Iterator<String> jsonObjKeyIter = jsonObj.keys();
-				while(jsonObjKeyIter.hasNext()) {
-					String jsonKey = jsonObjKeyIter.next();
-					this.manageJsonEntryObject(jsonKey, jsonObj.get(jsonKey), mapColumnHeader, listCells, 0, 0);
-				}
-				
-				/*for(Entry<String, Object> curEntry : jsonObj.toMap().entrySet()) {
-					this.manageJsonEntryObject(curEntry.getKey(), curEntry.getValue(), mapColumnHeader, listCells, 0, 0);
-				}*/
-			} else {
-				JSONArray jsonArray = new JSONArray(jsonStr);
-				this.manageJsonArrayObject(null, jsonArray, mapColumnHeader, listCells, 0);
+			String excelFileName = this.xlsxNameTextField.getText();
+			logger.info("Initialize the excel");
+			xlsMng = new ExcelManager(excelFileName.substring(0, excelFileName.lastIndexOf(".")));
+			for(File curJsonFile :  this.selectedJsonFiles) {				
+				String jsonStr = Files.readString(Path.of(curJsonFile.getAbsolutePath()));
+				//String sheetName = (excelFileName.length() > 31) ? excelFileName.substring(0, 31) : excelFileName;			
+				xlsMng.getJsonResult(curJsonFile.getName(), null, jsonStr);
 			}
-			
-			logger.debug(mapColumnHeader);
-			logger.debug(listCells);
 			
 		} catch (IOException e) {
 			logger.error(e);
 			e.printStackTrace();
+		} catch (XlsWrkSheetException e) {
+			logger.error(e);
+			e.printStackTrace();
+		} finally{
+			String targetPath = this.targetDirTextField.getText();
+			if(!targetPath.endsWith(File.separator)) {
+				targetPath += File.separator;
+			}
+			if(xlsMng != null) {
+				xlsMng.finalWrite(targetPath);
+				xlsMng = null;
+			}
+			JOptionPane.showMessageDialog(this.frame, "JSON Tabulation completed", "Conversion completed", JOptionPane.INFORMATION_MESSAGE);
 		}
 		logger.traceExit();
-	}
-	
-	private int manageJsonEntryObject(String key, Object value, Map<String, ColumnHeader> mapCols, List<CellHierarchy> listCells, int headerIdx, int arrayIdx) {
-		logger.traceEntry();
-		int depth = 0;
-		ColumnHeader columnHeader = null;
-		if(mapCols.containsKey(key)) {
-			columnHeader = mapCols.get(key);
-		} else {
-			columnHeader = new ColumnHeader();
-			columnHeader.name = key;
-			columnHeader.row = headerIdx;
-			columnHeader.column = mapCols.size();
-			mapCols.put(key, columnHeader);
-		}
-		
-		CellHierarchy cell = new CellHierarchy();
-		cell.row = arrayIdx;
-		cell.column = columnHeader.column;
-		listCells.add(cell);
-		
-		if(value instanceof JSONObject) {
-			
-			JSONObject jsonObj = (JSONObject) value;
-
-			Iterator<String> jsonObjKeyIter = jsonObj.keys();
-			while(jsonObjKeyIter.hasNext()) {
-				String jsonKey = jsonObjKeyIter.next();
-				this.manageJsonEntryObject(jsonKey, jsonObj.get(jsonKey), columnHeader.childrenCols, cell.listChild, headerIdx + 1, 0);
-			}
-			
-		} else if(value instanceof Map) {
-			logger.warn("instanceof Map");
-			Map<String, Object> objects = (Map<String, Object>) value;
-			for(Entry<String, Object> curEntry : objects.entrySet()) {
-				this.manageJsonEntryObject(curEntry.getKey(), curEntry.getValue(), columnHeader.childrenCols, cell.listChild, headerIdx + 1, 0);
-			}
-		} else if(value instanceof JSONArray) {
-			JSONArray jsonArray = (JSONArray) value;
-			this.manageJsonArrayObject(key, jsonArray, columnHeader.childrenCols, cell.listChild, headerIdx + 1);
-			
-		} else if(value instanceof List) {
-			logger.warn("instanceof List");
-			//List<Object> arrayObj = (List<Object>)value;
-			//this.manageJsonArrayObject(key, arrayObj, columnHeader.childrenCols, cell.listChild, headerIdx + 1);
-		} else {
-			cell.cellObject = value;
-		}
-		
-		
-		return logger.traceExit(depth);
-	}
-	
-	private int manageJsonArrayObject(String key, JSONArray jsonArray, Map<String, ColumnHeader> mapCols, List<CellHierarchy> listCells, int headerIdx) {
-		logger.traceEntry();
-		int depth = 0;
-		int idx = 0;
-		Iterator<Object> jsonArrayItr = jsonArray.iterator();
-		while(jsonArrayItr.hasNext()) {
-		
-			Object curObject = jsonArrayItr.next();
-		
-			if(curObject instanceof JSONObject) {
-				
-				JSONObject jsonObj = (JSONObject) curObject;
-
-				Iterator<String> jsonObjKeyIter = jsonObj.keys();
-				while(jsonObjKeyIter.hasNext()) {
-					String jsonKey = jsonObjKeyIter.next();
-					this.manageJsonEntryObject(jsonKey, jsonObj.get(jsonKey), mapCols, listCells, headerIdx, idx);
-				}
-				
-			} else if(curObject instanceof Map) {
-				logger.warn("instanceof Map");
-				Map<String, Object> objects = (Map<String, Object>) curObject;
-				for(Entry<String, Object> curEntry : objects.entrySet()) {
-					this.manageJsonEntryObject(curEntry.getKey(), curEntry.getValue(), mapCols, listCells, headerIdx, idx);
-				}
-			} else if(! (curObject instanceof List) && ! (curObject instanceof JSONArray) && !this.skipArrayCheckBox.isSelected()) {
-								
-				CellHierarchy cell = new CellHierarchy();
-				cell.row = idx;
-				cell.column = 0;
-				cell.cellObject = curObject;
-				listCells.add(cell);
-			}
-			idx++;
-		}
-		
-		return logger.traceExit(depth);
 	}
 }
