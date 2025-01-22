6c3fd525102bb3e42f3e2c54de7262b2f2785152
manuelmsperanza <manu.speranz@gmail.com>
2025-01-22 03:32:44 +0100

    fixed unhandled exception

--- PDFCreator/src/main/java/com/hoffnungland/poi/corner/pdfcreator/App.java
+++ PDFCreator/src/main/java/com/hoffnungland/poi/corner/pdfcreator/App.java
@@ -1,6 +1,7 @@
 package com.hoffnungland.poi.corner.pdfcreator;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,17 +17,19 @@ public class App
 {
 	private static final Logger logger = LogManager.getLogger(App.class);
 	
-    public static void main( String[] args )
-    {
-    	logger.traceEntry();
+	public static void main( String[] args )
+	{
+		logger.traceEntry();
 		try {
 			PdfWriter writer = new PdfWriter("Test.pdf", new WriterProperties().setPdfVersion(PdfVersion.PDF_2_0));
 			PdfDocument pdfDocument = new PdfDocument(writer);
-	    	pdfDocument.setTagged();
-	    	Document document = new Document(pdfDocument);
-	    	document.add(new Paragraph("Hello world!"));
-	    	document.close();
+			pdfDocument.setTagged();
+			Document document = new Document(pdfDocument);
+			document.add(new Paragraph("Hello world!"));
+			document.close();
 		} catch (FileNotFoundException e) {
+			logger.error(e.getMessage(), e);
+		} catch (IOException e) {
 			logger.error(e.getMessage(), e);
 		}
 		logger.traceExit();
