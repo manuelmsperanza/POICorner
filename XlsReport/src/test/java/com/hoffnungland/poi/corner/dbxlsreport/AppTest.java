b2e9eb21fd0b2dcd35a92ee2b3875484a9dc9aa8
manuelmsperanza <manu.speranz@gmail.com>
2026-04-14 17:13:10 +0200

    Fix Maven install path and upgrade build to Java 25

--- XlsReport/src/test/java/com/hoffnungland/poi/corner/dbxlsreport/AppTest.java
+++ XlsReport/src/test/java/com/hoffnungland/poi/corner/dbxlsreport/AppTest.java
@@ -1,13 +1,19 @@
 package com.hoffnungland.poi.corner.dbxlsreport;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class AppTest {
 
     @Test
-    void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void greetingReturnsExpectedMessage() {
+        assertEquals("Hello World!", App.greeting());
+    }
+
+    @Test
+    void worksheetExceptionStoresMessage() {
+        var exception = new XlsWrkSheetException("sheet missing");
+        assertEquals("sheet missing", exception.getMessage());
     }
 }
